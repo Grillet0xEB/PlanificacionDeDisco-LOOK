8c28efdd7adab1efc87cba9d67beaae60c547c2a
Carlos Grillet <carlosgrillet5000@hotmail.com>
2018-02-22 00:59:15 -0400

    Add files via upload

---
+++ AlgoritmoDePlanificacionDeDisco/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace AlgoritmoDePlanificacionDeDisco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(597, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Graficar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zedGraphControl1.IsShowPointValues = false;
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.PointValueFormat = "G";
+            this.zedGraphControl1.Size = new System.Drawing.Size(660, 360);
+            this.zedGraphControl1.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(107, 389);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(51, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Inicio del cabezal:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 391);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tira:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(178, 416);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Movimientos: ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 415);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Mostrar tira original";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 444);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.99D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOOK";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ AlgoritmoDePlanificacionDeDisco/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ZedGraph;
+using System.Collections;
+
+namespace AlgoritmoDePlanificacionDeDisco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            iniciar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Tira: |";
+            Graficar();
+            zedGraphControl1.Refresh();
+        }
+
+        private void iniciar()
+        {
+            GraphPane Grafico = zedGraphControl1.GraphPane;
+            Grafico.Title = "Algoritmo LOOK de Planificacion de Disco";
+            Grafico.FontSpec.FontColor = Color.Black;
+            Grafico.FontSpec.IsBold = true;
+            Grafico.Legend.IsVisible = false;
+            //Configuracion del panel
+            Grafico.PaneGap = 5;
+            Grafico.PaneFill = new Fill(Color.White, Color.LightBlue, 90f);
+            Grafico.PaneBorder = new Border(true, Color.RoyalBlue, 1.2f);
+            //Configuracion de los ejes
+            Grafico.AxisFill = new Fill(Color.White);
+            Grafico.AxisBorder = new Border(true, Color.Black, 1f);
+            //Configuracion del eje X
+            Grafico.XAxis.Title = "Pasos";
+            Grafico.XAxis.TitleFontSpec.FontColor = Color.Black;
+            Grafico.XAxis.TitleFontSpec.IsBold = false;
+            Grafico.XAxis.IsShowGrid = false;
+            Grafico.XAxis.IsShowMinorGrid = false;
+            Grafico.XAxis.MinorStep = 0.2f;
+            Grafico.XAxis.Min = 0;
+            Grafico.XAxis.Max = 16;
+            Grafico.XAxis.Step = 1;
+            //Configuracion del eje Y
+            Grafico.YAxis.Title = "Posicion del cabezal";
+            Grafico.XAxis.TitleFontSpec.FontColor = Color.Black;
+            Grafico.YAxis.TitleFontSpec.IsBold = false;
+            Grafico.YAxis.IsShowGrid = false;
+            Grafico.YAxis.IsShowMinorGrid = false;
+            Grafico.YAxis.MinorStep = 2f;
+            Grafico.YAxis.Min = 0;
+            Grafico.YAxis.Max = 99;
+            
+            zedGraphControl1.IsShowPointValues = false;
+            zedGraphControl1.Size = new Size(660, 360);
+            zedGraphControl1.AxisChange();
+        }
+
+        private void Graficar()
+        {
+            GraphPane Grafico = zedGraphControl1.GraphPane;
+            Grafico.GraphItemList.Clear();
+            Grafico.CurveList.Clear();
+            Grafico.Legend.IsVisible = checkBox1.Checked ? true : false;
+
+            PointPairList curvaOriginal = new PointPairList();
+            PointPairList curvaOrdenada = new PointPairList();
+            Llenar(curvaOriginal, curvaOrdenada); //Funcion que ordena la lista
+            
+            
+
+            if (checkBox1.Checked)
+            {
+                LineItem Movimientos = Grafico.AddCurve("FCFS",
+                                                    curvaOriginal,
+                                                    Color.Red,
+                                                    SymbolType.Circle);
+                Movimientos.Line.Width = 1.5f;
+                Movimientos.Line.IsSmooth = true;
+                Movimientos.Line.SmoothTension = 0.01f;
+                Movimientos.Symbol.Border.Color = Color.Black;
+
+                LineItem Movimientos2 = Grafico.AddCurve("LOOK",
+                                                    curvaOrdenada,
+                                                    Color.LightBlue,
+                                                    SymbolType.Circle);
+                Movimientos2.Line.Width = 1.5f;
+                Movimientos2.Line.IsSmooth = true;
+                Movimientos2.Line.SmoothTension = 0.01f;
+                Movimientos2.Symbol.Border.Color = Color.Black;
+            }
+            else
+            {
+                LineItem Movimientos = Grafico.AddCurve("LOOK",
+                                                 curvaOrdenada,
+                                                 Color.LightBlue,
+                                                 SymbolType.Circle);
+                Movimientos.Line.Width = 1.5f;
+                Movimientos.Line.IsSmooth = true;
+                Movimientos.Line.SmoothTension = 0.01f;
+                Movimientos.Symbol.Border.Color = Color.Black;
+            }
+
+        }
+
+        public PointPairList Llenar(PointPairList FCFS, PointPairList LOOK)
+        {
+            double[] puntosY = new double[17];
+            double[] puntosX = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+            double[] curvaDesendente = new double[17];
+            double[] curvaAscendente = new double[17];
+            puntosY[0] = (int)numericUpDown1.Value;
+            curvaDesendente[0] = (int)numericUpDown1.Value;
+            FCFS.Add(0, puntosY[0]);
+            int puntoMayor = 0;
+            int puntoMenor = 99;
+            int movimientos = 0;
+            Random rnd = new Random();
+            
+            for (int i = 1; i <= 16; i++)
+            {
+                int aleatorio = rnd.Next(0, 100);
+                label2.Text += String.Format(" {0} |", aleatorio);
+                FCFS.Add(i, aleatorio);
+
+                if (aleatorio <= puntosY[0])
+                {
+                    curvaDesendente[i] = aleatorio;
+                }
+                else
+                {
+                    curvaAscendente[i] = aleatorio;
+                }
+            }
+            
+            Array.Sort(curvaDesendente);
+            Array.Reverse(curvaDesendente);
+            Array.Sort(curvaAscendente);
+            for (int i = 1; i <= 16; i++)
+            {
+                puntosY[i] = curvaDesendente[i] + curvaAscendente[i];
+            }
+            for (int i = 0; i < puntosY.Length; i++)
+            {
+                if (puntosY[i] > puntoMayor)
+                {
+                    puntoMayor = (int)puntosY[i];
+                }
+
+                if (puntosY[i] < puntoMenor)
+                {
+                    puntoMenor = (int)puntosY[i];
+                }
+            }
+            LOOK.Add(puntosX, puntosY);
+            movimientos = ((int)puntosY[0] - puntoMenor) + (puntoMayor - puntoMenor);
+            label3.Text = String.Format("Movimientos = ({0} - {1}) + ({2} - {1}) = {3}", puntosY[0], puntoMenor, puntoMayor, movimientos);
+            
+            return LOOK;
+        }
+    }
+}
